46643078c18dc3e413c6e8adb0a2eb3a4a29681f
MayHa25 <may.hakimi010@gmail.com>
2025-05-18 16:03:49 +0300

    דף הרשמה 2

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,27 @@
-import { StrictMode } from 'react';
+// src/main.tsx
+import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App';
+import RegisterBusinessPage from './pages/public/RegisterBusinessPage';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/*" element={<App />} />
+        <Route path="/public/RegisterBusinessPage" element={<RegisterBusinessPage />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 );
 
 // רישום של Service Worker עבור PWA
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker
+      .register('/sw.js')
       .then((registration) => {
         console.log('SW registered: ', registration);
       })
