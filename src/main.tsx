4cb4012177b6ef268588acfe70a09a48415c0838
MayHa25 <may.hakimi010@gmail.com>
2025-05-18 16:31:45 +0300

    דף נחיתה 2

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,12 @@
 // src/main.tsx
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
-import RegisterBusinessPage from './pages/public/RegisterBusinessPage';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/*" element={<App />} />
-        <Route path="/public/RegisterBusinessPage" element={<RegisterBusinessPage />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
 
